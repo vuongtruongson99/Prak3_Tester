a97199f509d56281133d08ad20592023e75bcaa0
Truong Son <tr.son.2112@gmail.com>
2021-10-21 16:38:58 +0300

    First FT specced out in commnets, and now use unittest

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Edge('E:\\Ki 1 nam 3\\Tester\\Prak3\\edgedriver_win64\\msedgedriver.exe')
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Edge('E:\\Ki 1 nam 3\\Tester\\Prak3\\edgedriver_win64\\msedgedriver.exe')
+    
+    def tearDown(self):
+        self.browser.quit()
 
-assert 'successfully' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Check out its homepage of to-do list app
+        self.browser.get('http://localhost:8000')
+
+        # Notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
